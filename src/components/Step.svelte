a686c88035d5fdc0b2ea457857e8ba9d8262612e
Sean Widmer <bomberooski@gmail.com>
2024-04-30 16:51:27 -0400

    Update Step.svelte

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -11,10 +11,10 @@ gap-4 rounded-lg border border-solid border-zinc-700 text-center group
     </div>
     <h3 class="font-medium text-xl sm:text-2xl md:text-3xl">{step.name}</h3>
     <slot/>
-    <div class="flex-1 flex justify-between gap-4  items=center">
+    <div class="flex:1 flex justify-between gap-4  items=center">
         <div class="ml-auto cursor-pointer hover:text-emerald-900 duration-200 
         relative after:absolute after:top-0 after:h-0 after:right-full
-        after:w-full after:h-full after:bg-white after:duration-200 hover:after:translate-x-full after:z-[-1]
+        after:w-full after:h-full  after:bg-white after:duration-200 hover:after:translate-x-full after:z-[-1]
         overflow-hidden">
             <a class="relative z-4" href={step.href} target="_blank">Go to &rarr;</a>
         </div>
